33af3ac63bcd66c795950c77170ea187fb9d0eef
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-02-13 22:26:53 +0000

    second part is done
    convert the int/numbers into list.

--- e5.dart
+++ e5.dart
@@ -4,15 +4,20 @@
 
 // write a program that returns a list that contains only the elements that are common between them (without duplicates). Make sure your program works on two lists of different sizes.
 
-void main() {
-  List a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
-  List b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+List a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+List b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
+var x = <int>[];
+
+void main() {
   for (var i in a) {
     for (var j in b) {
       if (i == j) {
-        print(i);
+        x.add(i);
       }
     }
   }
+  print(x);
 }
+
+// List<dynamic> count() {}
