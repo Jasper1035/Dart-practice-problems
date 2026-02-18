e380942b37cf5ab294af7ed6579207aa97335834
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-02-18 19:04:19 +0000

    example no 8

---
+++ e8.dart
@@ -0,0 +1,12 @@
+// Make a two-player Rock-Paper-Scissors game against computer.
+
+//     Ask for player's input, compare them, print out a message to the winner.
+
+import 'dart:io';
+
+void main() {
+  stdout.write('Enter your term Rock/Paper/Scissor\n');
+  String? x = stdin.readLineSync();
+
+  print(x);
+}
