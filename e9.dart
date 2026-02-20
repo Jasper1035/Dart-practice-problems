f95c51497009887f6a0c60e3daf1622df8281714
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-02-20 19:44:55 +0000

    implement the conditions
    check the numbers is it too high or too low .. conditions.

--- e9.dart
+++ e9.dart
@@ -6,9 +6,21 @@ import 'dart:io';
 import 'dart:math';
 
 void main() {
-  stdout.write('Enter a random number\n');
+  stdout.write('Enter a random number between (0 - 100)\n');
   int? x = (int.parse(stdin.readLineSync()!));
-  print(x);
   var computerGuess = Random().nextInt(100);
   print(computerGuess);
+
+  int i = 0;
+  do {
+    if (x == computerGuess) {
+      print('You are Correct');
+      break;
+    } else if (x < computerGuess) {
+      print('Number is too low');
+    } else if (x > computerGuess) {
+      print('Number is too high');
+    }
+    i++;
+  } while (i == (x == computerGuess));
 }
