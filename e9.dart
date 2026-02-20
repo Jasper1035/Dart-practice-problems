600659bb4a7544ed057acd4af2f20965676eb751
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-02-20 19:28:46 +0000

    solve the first part
    takes the inputs from the user and generate a random num in console.

---
+++ e9.dart
@@ -0,0 +1,14 @@
+// Generate a random number between 1 and 100. Ask the user to guess the number, then tell them whether they guessed too low, too high, or exactly right.
+
+//     Keep track of how many guesses the user has taken, and when the game ends, print this out.
+
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  stdout.write('Enter a random number\n');
+  int? x = (int.parse(stdin.readLineSync()!));
+  print(x);
+  var computerGuess = Random().nextInt(100);
+  print(computerGuess);
+}
