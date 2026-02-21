a5ce615abf23dac8139418c17c2723cfb76acbaf
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-02-21 21:04:02 +0000

    solved

--- e10.dart
+++ e10.dart
@@ -11,11 +11,40 @@ void main() {
   print(func(x));
 }
 
+// int func(int x) {
+//   if (x % 2 == 0) {
+//     print('not prime');
+//   } else {
+//     print('prime');
+//   }
+
+//   return x;
+// }
+
 int func(int x) {
-  if (x % 2 == 0) {
+  // 1. Handle numbers less than 2 (0 and 1 are not prime)
+  if (x < 2) {
     print('not prime');
-  } else {
+    return x;
+  }
+
+  // 2. Assume it is prime until proven otherwise
+  bool isPrime = true;
+
+  // 3. The Condition: Loop from 2 up to x-1
+  for (int i = 2; i < x; i++) {
+    if (x % i == 0) {
+      // If x is divisible by any 'i', it is NOT prime
+      isPrime = false;
+      break; // Exit the loop early
+    }
+  }
+
+  // 4. Final check
+  if (isPrime) {
     print('prime');
+  } else {
+    print('not prime');
   }
 
   return x;
