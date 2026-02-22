51c75df33185b23044e73ed86f5ded7a12129b90
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-02-22 16:53:52 +0000

    List created

---
+++ e11.dart
@@ -0,0 +1,14 @@
+// Write a program that takes a list of numbers for example
+
+// a = [5, 10, 15, 20, 25]
+
+// and makes a new list of only the first and last elements of the given list. For practice, write this code inside a function.
+
+void main() {
+  List a = [5, 10, 15, 20, 25];
+  count(a);
+}
+
+List count(List a) {
+  return a;
+}
