f316a14101a6b7f3670664f2f9d87d5777128469
Jasper1035 <104673426+Jasper1035@users.noreply.github.com>
2026-03-02 17:18:08 +0000

    part 2 solved done

--- e18.dart
+++ e18.dart
@@ -65,3 +65,19 @@ List<List<int>> transpose(List<List<int>> board) {
     for (var i = 0; i < board.length; i++) [for (List<int> r in board) r[i]],
   ];
 }
+
+List<List<int>> diagonals(List<List<int>> board) {
+  /* 
+  Accepts: list of list of integers
+  Does: takes both diagonals and adds them to a new list
+  Returns: new list of lists
+  
+  Left-to-right diagonal is fairly easy.
+  To take the right-to-left diagonal, first we reverse the each row
+  then take left-to-right diagonal one more time
+  */
+  return [
+    [for (var i = 0; i < board.length; i++) board[i][i]],
+    [for (var i = 0; i < board.length; i++) board[i].reversed.toList()[i]],
+  ];
+}
